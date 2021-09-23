19546ff9558f7094f5fecfd597a937a94c81fc6c
zzwzdcw <2749978755@qq.com>
2021-09-23 23:21:00 +0800

    简略的layout

--- app/src/main/java/com/my/lines/MainActivity.java
+++ app/src/main/java/com/my/lines/MainActivity.java
@@ -3,12 +3,23 @@ package com.my.lines;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button start,save,add,mod;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        save=findViewById(R.id.save);
+        start=findViewById(R.id.start);
+        add=findViewById(R.id.add);
+        mod=findViewById(R.id.mod);
+        imageView=findViewById(R.id.imageview);
     }
+
+
+
 }
